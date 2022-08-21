ffa38d2fc99ba48e0490a6705bdb8a5b9580a2e7
jmjmjames <chungjm0711@gmail.com>
2022-08-21 10:14:27 +0900

    Chore: 프로젝트 세팅 (#1)

---
+++ module-common/src/test/java/com/jongmin/modulecommon/ModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongmin.modulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
